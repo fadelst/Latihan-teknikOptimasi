3106e14552ef6892160c0c73dfce813b638eda83
fadel syahabana <49062190+fadelst@users.noreply.github.com>
2022-01-08 14:41:14 +0700

    Add files via upload

---
+++ pertemuan10/app/Population.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'app/Chromosome.php';
+class InitialPopulation
+{
+    function generatePopulation(int $popSize, array $catalogue, int $numOfGen): array
+    {
+        $population = [];
+        $chromosome = new Chromosome($catalogue);
+        if ($popSize > 0) {
+            for ($i = 0; $i < $popSize; $i++) {
+                $population[] = $chromosome->createChromosome($numOfGen);
+            }
+        }
+        return $population;
+    }
+}
